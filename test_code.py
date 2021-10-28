3ac7c6049803933a4118637fcab6a405a2b787f2
gong0062 <gong0062@umn.edu>
2021-10-28 12:06:55 -0500

    Update test_code.py

--- test_code.py
+++ test_code.py
@@ -8,7 +8,7 @@ Created on Fri Sep  3 15:23:21 2021
 import ube3_apa
 
 
-working_dir = "C:/Users/gy199/OneDrive/Desktop/E3_ligase_enrichment/"
+working_dir = ""#directory of folder "testdata"
 ube3_apa.e3enrich(working_dir+"testdata/siteratio_testdata.csv","UniprotAC",working_dir+"testdata",exp_label="1",grouped=False,ratio_output=True,log2trans=False)
 ube3_apa.e3enrich(working_dir+"testdata/siteratio_testdata.csv","UniprotAC",working_dir+"testdata",exp_label="2",grouped=True,ratio_output=True,log2trans=False)
 ube3_apa.e3enrich(working_dir+"testdata/siteratio_testdata.csv","UniprotAC",working_dir+"testdata",exp_label="3",proratio_dir=working_dir+"testdata/proteinratio_testdata.csv",grouped=False,ratio_output=True,log2trans=False)
